beb681d9c96683c01cd61c6c329af5399cff35ef
Darrell Satriano <darrellsatriano@Darrells-MacBook-Pro-2.local>
2026-05-10 22:39:26 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        entry: "/src/server.ts",
-      }
-    }),
+    tanstackStart(),
+    nitro(),
     react(),
-  ]
+  ],
 });
